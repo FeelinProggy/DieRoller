a23d27e2eaa8e49006a841cabc39638e9610bb84
Jennifer Peterson <jennpeterson986@gmail.com>
2024-01-16 19:40:10 -0800

    Add project files.

---
+++ Die.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DielRoller
+{
+    /// <summary>
+    /// Represents a single six-sided die (1-6)
+    /// </summary>
+    public class Die
+    {
+        /// <summary>
+        /// Current face value of the die/user roll
+        /// </summary>
+        public byte FaceValue { get; set; }
+
+        /// <summary>
+        /// True if the current die value is held
+        /// </summary>
+        public bool IsHeld { get; set; }
+
+        /// <summary>
+        /// Rolls the die and returns the new "rolled" value
+        /// to set the <see cref="FaceValue"/> property
+        /// </summary>
+        public byte Roll()
+        {
+            // Generate a random number
+            // Set the FaceValue property
+            // Return the new value
+            throw new NotImplementedException();
+            
+        }
+    }
+}
